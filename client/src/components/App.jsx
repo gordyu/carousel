c50b967c588ba3816e31ffb43859d8df7acf8c39
Sarkis-S <Sarkis.Sos19@gmail.com>
2019-05-27 18:12:37 -0700

    Reimplement a new solution to carousel. This is new code and uses basic scss for styling. Remove Photo component. This is a working state of carousel.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import Photos from './Photos.jsx';
+import Carousel from './Carousel.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,32 +10,10 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Photos />
+        <Carousel />
       </div>
     );
   }
 }
 
 export default App;
-
-/*
-    return (
-      <div>
-        <div className="leftDiv">
-          <img src="https://s3-us-west-1.amazonaws.com/propimage55/0.webp" width="50%"></img>
-
-          <div className="rightDiv">
-            <div>
-              <img src="https://s3-us-west-1.amazonaws.com/propimage55/1.webp" height="20%" width="20%"></img>
-              <img src="https://s3-us-west-1.amazonaws.com/propimage55/3.webp" height="20%" width="20%"></img>
-            </div>
-            <div>
-              <img src="https://s3-us-west-1.amazonaws.com/propimage55/1.webp" height="20%" width="20%"></img>
-              <img src="https://s3-us-west-1.amazonaws.com/propimage55/3.webp" height="20%" width="20%"></img>
-            </div>
-          </div>
-          
-        </div>
-      </div>
-    );
-*/
