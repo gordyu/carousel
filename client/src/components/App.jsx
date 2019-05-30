812ec2dfca60200a2fc8206e2e5b830a10e45ae1
Sarkis-S <Sarkis.Sos19@gmail.com>
2019-05-30 16:05:33 -0700

    Modal is in working state but requires more styling.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import Carousel from './Carousel.jsx';
+import Modal from './Modal.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -9,15 +9,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="wrapper">
-        <div className="box a"><img src="https://s3-us-west-1.amazonaws.com/propimage55/0.webp" height="435" width="750"></img></div>
-        <div className="box b">
-          <div className="box c"><img src="https://s3-us-west-1.amazonaws.com/propimage55/1.webp" height="213" width="380"></img></div>
-          <div className="box d"><img src="https://s3-us-west-1.amazonaws.com/propimage55/2.webp" height="213" width="380"></img></div>
-          <div className="box e"><img src="https://s3-us-west-1.amazonaws.com/propimage55/3.webp" height="213" width="380"></img></div>
-          <div className="box f"><img src="https://s3-us-west-1.amazonaws.com/propimage55/4.webp" height="213" width="380"></img></div>
-        </div>
-      </div>
+      <Modal />
     );
   }
 }
